8bcc5633ba293633fd907bfe02e0d4b926ac7f88
Jonathan Muniz <jsmuniz7@yahoo.com.br>
2013-02-03 19:21:04 -0200

    Primeiro Commit

---
+++ desamb.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="estilo.css" />
+<title>Copa Brasil 2014 Framenet</title>
+</head>
+
+<body>
+<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+	<div class="global-div">
+    
+    	<div class="topo-div"> 
+        <ul>
+				<li><a href="index.html" title="Português">Português <img alt="Português" src="Imagens/bandeira1.jpg" height="11" width="20" /></a></li>
+				<li><a href="index_en.html" title="Inglês">Inglês <img alt="Inglês" src="Imagens/bandeira2.jpg" height="11" width="20" /></a></li>
+				<li><a href="#" title="Espanhol">Espanhol <img alt="Espanhol" src="Imagens/bandeira3.jpg" height="11" width="20" /></a></li>                                              
+        </ul>
+        </div>
+        
+        <div class="banner-div"> <img alt="Banner Copa" src="Imagens/banner1.jpg" height="230" width="940" /> </div>
+        
+        <div class="menu-div">
+        	<ul>
+				<li><a href="index.html" title="Home">Página Principal</a></li>
+				<li><a href="#" title="Sobre">Sobre o Dicionário</a></li>
+				<li><a href="#" title="How To">Como Consultar</a></li>
+				<li><a href="#" title="Contato">Contato</a></li>                                                
+            </ul>
+            
+        </div>
+        
+        <div class="pesquisa-div">
+        
+        <form action="desamb.php">
+        
+        <input type="text" name="nome" size="25" value="Insira uma Palavra" class="pesquisa" />
+          
+        <input type="submit" name="submeter" value="Ver Definição" class="botao" /> 
+        
+        </form>
+        
+        </div>
+        
+        <div class="esq-div">
+        
+        	<div class="definicao-div">
+            	<h5>BUSCA</h5>
+                <p class="right17"><b>Marcar</b> pode se referir a:</p>
+                <hr />
+                <ul class="right17">
+                <li><a href="#"><img src="Imagens/bandeira1.jpg" height="11" width="20"> <p>[Fazer_Gol]</p></a></li>
+                <li><a href="#"><img src="Imagens/bandeira2.jpg" height="11" width="20"><p>[Score]</p></a></li>
+                <li><a href="#"><img src="Imagens/bandeira3.jpg" height="11" width="20"><p>[Marcar_Golo]</p></a></li>
+                </ul>
+                <hr />
+                <ul class="right17">
+                <li><a href="#"><img src="Imagens/bandeira1.jpg" height="11" width="20"> <p>[Punição]</p></a></li>
+                <li><a href="#"><img src="Imagens/bandeira2.jpg" height="11" width="20"><p>[Punishment]</p></a></li>
+                </ul>
+                <hr />
+                <ul class="right17">
+                <li><a href="#"><img src="Imagens/bandeira1.jpg" height="11" width="20"> <p>[Confirmar_Gol]</p></a></li>
+                </ul>
+                <hr />
+             </div>
+        </div>
+        
+        <div class="dir-div">
+        
+        	<div class="maisacessados-div">
+            	<h4>Mais Acessados</h4>
+            	<ul class="maisartigos escuro top8">
+            	<li><a href="#" title="Gol">Gol</a></li>
+            	<li><a href="#" title="Chute">Chute</a></li>
+            	<li><a href="#" title="Falta">Falta</a></li>
+            	<li><a href="#" title="Juiz">Juiz</a></li>
+            	<li><a href="#" title="Estádio">Estádio</a></li>
+            	<li><a href="#" title="Bola">Bola</a></li>
+            	<li><a href="#" title="Pénalti">Pênalti</a></li>
+            	</ul>
+           </div>
+           
+           <div class="anuncios-div"> <img alt="Publicidade" src="Imagens/publicidade1.jpg" /> </div> 
+           <div class="anuncios-div left17"> <img alt="Publicidade" src="Imagens/publicidade3.jpg" />  </div> 
+           <div class="anuncios-div "> <img alt="Publicidade" src="Imagens/publicidade4.jpg" />  </div> 
+           <div class="anuncios-div left17"> <img alt="Publicidade" src="Imagens/publicidade2.jpg" /> </div>
+         <div class="clear"> </div>   
+        </div> 
+       
+        
+        <div class="redesocial-div" style="text-align:center">
+        
+        <p> Compartilhe </p>      
+        		
+     	<div class="facebook"><div class="fb-like" data-href="http://www.dossiebrasil.xpg.com.br" data-send="true" data-layout="button_count" data-width="450" data-show-faces="true" data-font="arial"></div> </div>
+        
+        <div class="twitter"> <a href="https://twitter.com/share" class="twitter-share-button" data-lang="pt">Tweetar</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script> </div>
+        
+        <div class="plus"><g:plusone size="medium"></g:plusone> </div>
+        
+        </div>
+        
+        <div class="rodape-div">
+          <p>UFJF - 20110</p></div>
+</div>        
+</body>
+</html>
